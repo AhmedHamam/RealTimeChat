54a8e7b75f3f7d5ff457f8aa3c3c3cf9aa98cd87
ahmed ali ahmed <ahmed.ali1993@hotmail.com>
2023-02-05 16:05:17 +0200

    Start Project

--- RealTimeChat/RealChat.Api/Controllers/IdentityController.cs
+++ RealTimeChat/RealChat.Api/Controllers/IdentityController.cs
@@ -1,6 +1,34 @@
-﻿namespace RealChat.Api.Controllers
+﻿using Base.API.Controllers;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealChat.Application.Commands.Register;
+
+namespace TicketManagement.Identity.API.Controllers
 {
-    public class IdentityController
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class IdentityController : BaseController
     {
+        public IdentityController(ISender mediator) : base(mediator)
+        {
+        }
+
+        #region Commands
+        [AllowAnonymous]
+        [HttpPost("register")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Guid?))]
+        public async Task<ActionResult<Guid?>> RegisterUser(RegisterCommand command)
+        {
+            return Ok(await Mediator.Send(command, CancellationToken));
+        }
+
+
+
+
+        #endregion
     }
 }
---
+++ RealTimeChat/RealChat.Application/Commands/Register/RegisterValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace RealChat.Application.Commands.Register
+{
+    public class RegisterValidator : AbstractValidator<RegisterCommand>
+    {
+        public RegisterValidator()
+        {
+            RuleFor(x => x.Email).NotEmpty().EmailAddress();
+            RuleFor(x => x.Password).NotEmpty().MinimumLength(6);
+            RuleFor(x => x.FirstName).NotEmpty();
+            RuleFor(x => x.LastName).NotEmpty();
+        }
+    }
+}
